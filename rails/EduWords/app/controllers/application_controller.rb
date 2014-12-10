8ab4f2f59f85d3dd10a4758776b46d520bd638a1
Daniel Sienkiewicz <henio180@interia.pl>
2014-12-10 14:43:59 +0100

    #39 auto language detection

--- rails/EduWords/app/controllers/application_controller.rb
+++ rails/EduWords/app/controllers/application_controller.rb
@@ -13,8 +13,14 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
 
+ private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    #I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
   end
   
   def default_url_options(options = {})
