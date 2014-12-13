8712ae2e04051429536122158ea5961c5c86ffe2
Daniel Sienkiewicz <henio180@interia.pl>
2014-12-13 11:52:58 +0100

    #39 preparation to multilanguage version

--- rails/EduWords/app/controllers/application_controller.rb
+++ rails/EduWords/app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     #I18n.locale = params[:locale] || I18n.default_locale
-    I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
   end
   
   def default_url_options(options = {})
